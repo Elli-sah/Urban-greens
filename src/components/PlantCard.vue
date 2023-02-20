3beaf893e814f2d2b2646deadc09423afef4232d
Tove Karlström <tovekarlstrom@hotmail.se>
2023-02-20 13:28:03 +0100

    Functionality in PlantCard

--- src/components/PlantCard.vue
+++ src/components/PlantCard.vue
@@ -10,8 +10,30 @@
 </script>
 
 <template>
-  <h1>hej</h1>
-  <ul>
-    <li>{{ plant.name }}</li>
-  </ul>
+  <div class="plantBox">
+    <img alt="" :src="plant.image" />
+    <p>{{ plant.name }}</p>
+  </div>
 </template>
+
+<style scoped>
+  .plantBox {
+    width: 300px;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: antiquewhite;
+    margin: 10px;
+  }
+  img {
+    width: 200px;
+    height: 300px;
+
+    object-fit: cover;
+  }
+  p {
+    font-weight: bold;
+  }
+</style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,10 @@
 <script>
   import axios from 'axios'
+  import PlantCard from '../components/PlantCard.vue'
   export default {
+    components: {
+      PlantCard
+    },
     created() {
       this.axiosGetPlants()
     },
@@ -23,7 +27,18 @@
 
 <template>
   <h1>ProduktListan</h1>
-  <ul v-for="plant in result" :key="plant">
-    <li>{{ plant.name }}</li>
-  </ul>
+  <div>
+    <PlantCard v-for="plant in result" :key="plant" :plant="plant" />
+  </div>
 </template>
+
+<style>
+  div {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: start;
+    gap: 10px;
+    width: 100%;
+    justify-content: center;
+  }
+</style>
