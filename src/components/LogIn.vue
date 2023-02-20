cb4a0fbeb3d6f76b0d83930d101c69da7476209a
Klara Bryntesson <k_bryntesson@hotmail.com>
2023-02-20 12:46:30 +0100

    Add code for login and Vuex

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,12 +1,44 @@
-<script></script>
+<script>
+  import { mapState } from 'vuex'
+  export default {
+    data() {
+      return { userName: '', password: '', users: null, showMessage: false }
+    },
+    created() {
+      this.showMessage = false
+    },
+    computed: {
+      ...mapState({
+        myState: (state) => state.users
+      })
+    },
+    methods: {
+      AtLogin() {
+        this.users = this.myState
+        if (
+          this.users[this.userName] !== undefined &&
+          this.users[this.userName].password === this.password
+        ) {
+          console.log('hej')
+        } else {
+          console.log('hejdå')
+          this.showMessage = true
+        }
+      }
+    }
+  }
+</script>
 
 <template>
   <h1>Logga in</h1>
-  <form>
-    <label>Användarnamn <input :v-model="userName" /></label>
-    <label>Lösenord<input type="password" :v-model="password" /></label>
+  <form @submit.prevent="AtLogin">
+    <label>Användarnamn <input v-model="userName" /></label>
+    <label>Lösenord<input type="password" v-model="password" /></label>
     <button type="submit">Logga in</button>
   </form>
+  <div v-if="showMessage">
+    <p>Du verkar inte ha något konto!</p>
+  </div>
 </template>
 
 <style></style>
