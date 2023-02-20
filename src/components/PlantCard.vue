d5e36ec547c715371dae41c630c177a0c4aa88bf
Tove Karlström <tovekarlstrom@hotmail.se>
2023-02-20 11:40:50 +0100

    imported PlantCard

--- src/components/PlantCard.vue
+++ src/components/PlantCard.vue
@@ -1,5 +1,17 @@
-<script></script>
+<script>
+  export default {
+    props: {
+      plant: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  }
+</script>
 
 <template>
   <h1>hej</h1>
+  <ul>
+    <li>{{ plant.name }}</li>
+  </ul>
 </template>
