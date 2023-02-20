57d1a88b0ad1f82d1e3e5560de2d2bfb761078b9
Tove Karlström <tovekarlstrom@hotmail.se>
2023-02-20 11:11:33 +0100

    Fetching names from json

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,29 @@
-<template><h1>Hem</h1></template>
+<script>
+  import axios from 'axios'
+  export default {
+    created() {
+      this.axiosGetPlants()
+    },
+
+    data() {
+      return {
+        result: ''
+      }
+    },
+
+    methods: {
+      axiosGetPlants() {
+        axios.get('/plants.json').then((response) => {
+          this.result = response.data
+        })
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>ProduktListan</h1>
+  <ul v-for="plant in result" :key="plant">
+    <li>{{ plant.name }}</li>
+  </ul>
+</template>
