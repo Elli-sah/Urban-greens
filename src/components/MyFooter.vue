08a07a52c3e44b9716b7f277b27cbaf457059b94
Ellinor Sahlberg <ellinorsahlberg1@gmail.com>
2023-02-21 11:18:24 +0100

    updated footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -4,60 +4,107 @@
 </script>
 
 <template>
-  <footer id="footer">
-    <div id="container">
-      <div id="h1">
-        <h1>Logga?</h1>
-      </div>
-      <div id="allFooterCat">
-        <div id="about">
-          <h4>Home</h4>
-          <ul id="aboutUl">
-            <li><a href="#">About Brand Name</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Jobba hos oss</a></li>
+  <footer class="text-center text-lg-start">
+    <div class="container p-4">
+      <h1 class="row d-flex justify-content-start">Logga?</h1>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Home</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
           </ul>
         </div>
-        <div id="about">
-          <h4>Brand name</h4>
-          <ul id="aboutUl">
-            <li><a href="#">About Brand Name</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Jobba hos oss</a></li>
+
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Brand name</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
           </ul>
         </div>
-        <div id="about">
-          <h4>About</h4>
-          <ul id="aboutUl">
-            <li><a href="#">About Brand Name</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Jobba hos oss</a></li>
+
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">About</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
           </ul>
         </div>
-        <div id="contact">
-          <h4>Contact</h4>
-          <ul id="contactUl" class="list-unstyled">
-            <li>Brand Name AB</li>
-            <li>blablagatan 5</li>
-            <li>543 23 Göteborg</li>
-            <li>Email: Greenfingers@gmail.com</li>
+
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Contact</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
           </ul>
         </div>
       </div>
     </div>
-    <div>
+
+    <div class="container p-4 pb-0">
       <section class="">
         <form action="">
-          <div>
-            <div>
+          <div class="row d-flex justify-content-center">
+            <div class="col-auto">
               <p class="pt-2">
                 <strong>Sign up for our newsletter</strong>
               </p>
             </div>
 
-            <div id="form" class="col-md-5 col-12">
+            <div class="col-md-5 col-12">
               <div class="form-outline form-white mb-4">
-                <input type="email" id="form5Example29" class="form-control" />
+                <input
+                  type="email"
+                  id="newsletter"
+                  class="form-control"
+                  placeholder="Email"
+                />
               </div>
             </div>
 
@@ -66,94 +113,20 @@
                 Subscribe
               </button>
             </div>
-            <div class="col-md-6">
-              <ul id="policy" class="list-unstyled">
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-              </ul>
-            </div>
           </div>
+          <!-- <div id="policy">
+            <ul class="list-unstyled">
+              <li><a href="#">Terms of Service</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div> -->
         </form>
       </section>
     </div>
 
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-      © 2023 Copyright: Brand-Name AB
+      © 2020 Copyright:
+      <a class="text-dark" href="#">Brand Name</a>
     </div>
   </footer>
 </template>
-
-<style>
-  #footer {
-    width: 1440px;
-    height: auto;
-    font-family: poiret;
-    background-color: rgba(49, 109, 24, 0.485);
-  }
-
-  footer p {
-    font-size: 0.9rem;
-    margin-bottom: 1rem;
-  }
-
-  footer ul li {
-    display: inline-block;
-    margin-right: 1rem;
-  }
-
-  footer ul li a {
-    color: black;
-    text-decoration: none;
-  }
-
-  #nameIcon {
-    display: flex;
-    flex-direction: column;
-    width: fit-content;
-  }
-
-  #contact {
-    display: flex;
-    flex-direction: column;
-    width: fit-content;
-  }
-
-  #contactUl {
-    display: flex;
-    flex-direction: column;
-    padding: 0;
-  }
-
-  #about {
-    display: flex;
-    flex-direction: column;
-    width: fit-content;
-  }
-
-  #allFooterCat {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-  }
-
-  #aboutUl {
-    display: flex;
-    flex-direction: column;
-    padding: 0;
-  }
-
-  #form {
-    margin-left: 70px;
-  }
-
-  #h1 {
-    display: flex;
-    justify-content: left;
-    margin: 30px;
-  }
-
-  @font-face {
-    font-family: poiret;
-    src: url(../../assets/Poiret_One/PoiretOne-Regular.ttf);
-  }
-</style>
