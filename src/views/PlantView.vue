bf6fa929c7289b9260cf7d6dc168852f2a895688
Klara Bryntesson <k_bryntesson@hotmail.com>
2023-02-21 10:36:06 +0100

    mutations in vuex and method in plantview

--- src/views/PlantView.vue
+++ src/views/PlantView.vue
@@ -23,6 +23,13 @@
           )[0]
           console.log(this.plant)
         })
+      },
+      atAddPlant() {
+        this.$store.commit('addPlant', {
+          user: 'klarab',
+          addplant: this.plant
+        })
+        console.log(this.plant)
       }
     },
     created() {
@@ -39,5 +46,6 @@
     <p>{{ plant.placement.short }}</p>
     <p>{{ plant.temperature.short }}</p>
     <p>{{ plant.watering.short }}</p>
+    <button @click="atAddPlant">Lägg till på fönsterbrädan</button>
   </div>
 </template>
