06417c3f6fd857d2bdb06fb8a7bfff7ea3f5e55f
Klara Bryntesson <k_bryntesson@hotmail.com>
2023-02-21 12:14:38 +0100

    Update profile and mapstate

--- src/components/MyShelf.vue
+++ src/components/MyShelf.vue
@@ -1,7 +1,27 @@
-<script></script>
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    props: {
+      profile: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    computed: {
+      ...mapState({
+        loggedInUser: (state) => state.loggedInUser,
+        myPlants: (state) => state.users[this.loggedInUser.user].favorites
+      })
+    }
+  }
+</script>
 
 <template>
   <h1>hej</h1>
+  <!-- <ul v-if="myPlants.length !== 0">
+    <li :key="plant.name" v-for="plants in myPlants" />
+  </ul> -->
 </template>
 
 <style></style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,21 +3,25 @@
   import { mapState } from 'vuex'
 
   export default {
+    data() {
+      return { profile: null }
+    },
     components: {
       MyShelf
     },
     computed: {
       ...mapState({
-        myPlants: (state) => state.users[this.loggedInUser],
+        myProfile: (state) => state.users[this.loggedInUser.user],
         loggedInUser: (state) => state.loggedInUser
       })
+      this.profile = this.loggedInUser
     }
   }
 </script>
 
 <template>
   <h1>Här är din fönsterbräda, {{ loggedInUser.name }}!</h1>
-  <MyShelf />
+  <MyShelf :profile="myProfile" />
 </template>
 
 <style></style>
