06a2a5eea928ee8196c0f4550d5440a7ddef114f
Tove Karlström <tovekarlstrom@hotmail.se>
2023-02-24 12:27:34 +0100

    Update

--- src/views/GetStartedView.vue
+++ src/views/GetStartedView.vue
@@ -14,13 +14,41 @@
   <div class="view-divs">
     <div id="info">
       <h1>Kom Igång</h1>
-      <p>
+      <p class="dd">
         Få kontroll över ditt växtsamhälle! Ta hand om dina gröna vänner och se
         dem blomstra med hjälp av vår app. Kontakta oss om du behöver hjälp.
         Lycka till med dina växter!
       </p>
     </div>
     <MyShelfInfo />
+    <div id="bb">
+      <div class="cc">
+        <p class="dd">
+          Få kontroll över ditt växtsamhälle! Ta hand om dina gröna vänner och
+          se dem blomstra med hjälp av vår app. Kontakta oss om du behöver
+          hjälp. Lycka till med dina växter!
+        </p>
+        <p class="tt">01</p>
+      </div>
+      <div class="cc">
+        <p class="tt">02</p>
+
+        <p class="dd">
+          Få kontroll över ditt växtsamhälle! Ta hand om dina gröna vänner och
+          se dem blomstra med hjälp av vår app. Kontakta oss om du behöver
+          hjälp. Lycka till med dina växter!
+        </p>
+      </div>
+      <div class="cc">
+        <p class="dd">
+          Få kontroll över ditt växtsamhälle! Ta hand om dina gröna vänner och
+          se dem blomstra med hjälp av vår app. Kontakta oss om du behöver
+          hjälp. Lycka till med dina växter!
+        </p>
+        <p class="tt">03</p>
+      </div>
+    </div>
+
     <StepByStep />
     <RouterLink to="/login">
       <button id="komIgang" class="button">Logga in</button>
@@ -29,6 +57,21 @@
 </template>
 
 <style scoped>
+  #bb {
+    max-width: 600px;
+    padding: 50px;
+    border-radius: 20px 0 20px 0;
+    background-color: beige;
+  }
+  .dd {
+    font-size: 1.2em;
+  }
+  .cc {
+    display: flex;
+  }
+  .tt {
+    font-size: 3em;
+  }
   #info {
     background-color: #577751;
     border-radius: 20px;
