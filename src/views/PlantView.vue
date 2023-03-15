dc46c6f4beedd6ad14b754752c64747a2df6413c
sannaasp <sanna.asp@hotmail.com>
2023-03-15 09:36:42 +0100

    update comments

--- src/views/PlantView.vue
+++ src/views/PlantView.vue
@@ -64,13 +64,14 @@
           }
         })
       },
-
+      // Funktion för att visa info om varje ikon på varje blomma
       openModal(modalInfo) {
         this.selectedPlant = this.plant
         this.plantHeading = this.plant[modalInfo].title
         this.longDescription = this.plant[modalInfo].description
         document.body.style.overflow = 'hidden'
       },
+      // Funktion för att stänga informationsrutan
       closeModal() {
         this.selectedPlant = null
         this.NotLoggedIn = false
